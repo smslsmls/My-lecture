a624f6065fd96aa19f44b1d33fc6ed98ec297a6a
윤서준 <skipp1223@naver.com>
2021-03-09 11:13:00 +0900

    comit

---
+++ 비교연산자/소스.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+	int n1 = 1, n2 = 2;
+	int result = 0;
+	printf("result = %d\n", result);
+	result = (n1 == n2);
+	printf("result = %d\n", result);
+	return 0;
+}
