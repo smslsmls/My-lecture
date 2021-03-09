4d0b3c72d504f4ffe553065cb6496671f2264ed0
윤서준 <skipp1223@naver.com>
2021-03-09 11:43:10 +0900

    zjalt

--- My lecture/소스.c
+++ My lecture/소스.c
@@ -1,11 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
-int main() {
+void pm() {
 	int n1 = 1;
 	printf("n1 = %d", n1);
 	n1++;
 	printf("n1 = %d", n1++);
 	printf("n1 = %d", n1);
 
-	return 0;
+	return;
+}
+void comp() {
+	int n1 = 1, n2 = 2;
+	int result = 0;
+	printf("result = %d\n", result);
+	result = (n1 == n2);
+	printf("result = %d\n", result);
+
+	n2 = 1;
+	result = (n1 == n2);
+	printf("result = %d\n", result);
+
+	return;
+}
+int main() {
+
 }
