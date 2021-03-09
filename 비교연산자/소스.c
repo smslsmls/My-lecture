4da907ff59ae8826def09fdc66c7ef38bf6c31cd
윤서준 <skipp1223@naver.com>
2021-03-09 11:22:19 +0900

    zjalf

--- 비교연산자/소스.c
+++ 비교연산자/소스.c
@@ -6,5 +6,10 @@ int main() {
 	printf("result = %d\n", result);
 	result = (n1 == n2);
 	printf("result = %d\n", result);
+
+	n2 = 1;
+	result = (n1 == n2);
+	printf("result = %d\n", result);
+
 	return 0;
 }
