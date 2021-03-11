336c86890137de00ad1e6e673f7d587dd1f5455b
user <user@DESKTOP-ATMEM49>
2021-03-11 16:10:39 +0900

    zjalt

--- My lecture/c class 2.c
+++ My lecture/c class 2.c
@@ -19,9 +19,37 @@ int p2() {
 	int a, b, c, d;
 	scanf("%d %d %d %d", &a, &b, &c, &d);
 	printf("sum %d\navg %d", a + b + c + d, (a + b + c + d) / 4);
+	return 0;
 }
+
+int p3() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d / %d = %d...%d", a, b, a / b, a % b);
+	return 0;
+}
+
+int p4() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("width = %d\nlength = %d\narea = %d", a += 5, b *= 2, a * b);
+}
+
+int p5() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d %d\n", ++a, b);
+	printf("%d %d", a, --b);
+}
+
+int p6() {
+	int a, b, c, d;
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	printf("%d",((a > c) && (b > d)));
+}
+
 int main() {
-	p2();
+	p6();
 
 	return 0;
 }
