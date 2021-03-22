9598783c358ee3f98f974f2bfe5c63535734837f
smslsmls <skipp1223@naver.com>
2021-03-22 12:04:38 +0900

    zjalt

--- My lecture/c class 1.c
+++ My lecture/c class 1.c
@@ -107,10 +107,29 @@ int ascii() {
 }
 //change type
 int ch() {
-	int n1, n2;
+	int n1 = 1, n2 = 2;
 	double n;
 	n = n1 / n2;
 	n = (double)n1 / n2;
+
+	return 0;
+}
+//format specifier(서식 지정자)
+int format() {
+	double d1 = 1.23e-3;
+	printf("%f\n", d1);
+	printf("%e\n", d1);
+	printf("%g\n", d1);
+	double d2 = 1.234567e-3;
+	printf("%f\n", d2);
+	printf("%e\n", d2);
+	printf("%g\n", d2);
+	double d3 = 0.01234567;
+	printf("%f\n", d3);
+	printf("%e\n", d3);
+	printf("%g\n", d3);
+
+	return 0;
 }
 
 
@@ -170,7 +189,7 @@ int p6() {
 
 
 int main() {
-	p6();
+	format();
 
 	return 0;
 }
