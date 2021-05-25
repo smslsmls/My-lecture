e448773ed913cd92e2d21ad9c0e4506389160aee
서준 윤 <46644291+smslsmls@users.noreply.github.com>
2021-05-25 17:47:39 +0900

    zjatl

--- My lecture/maze.c
+++ My lecture/maze.c
@@ -15,20 +15,30 @@ int Si, Sj, Ei, Ej, succes, top, ri[100], rj[100];
 int X[4] = { 1,0,-1,0 };
 int Y[4] = { 0,1,0,-1 };
 int visit(int, int);
-void gotoxy(int x, int y) {
-	COORD xy = { x,y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), xy);
-}
+void print(int, int);
+void CursorView();
+void gotoxy(int, int);
 int main() {
 	top = 0;
 	succes = 0;
 	printf("=== find maze path game ===\n");
 	Si = 1; Sj = 1;
 	Ei = 7; Ej = 7;
+	CursorView();
 	visit(Si, Sj);
 	gotoxy(0, 10);
-
 	return 0;
+}
+void gotoxy(int x, int y) {
+	COORD xy = { x,y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), xy);
+}
+void CursorView()
+{
+	CONSOLE_CURSOR_INFO cursorInfo = { 0, };
+	cursorInfo.dwSize = 1;
+	cursorInfo.bVisible = FALSE;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
 }
 void print(int i, int j) {
 	gotoxy(0, 1);
@@ -39,7 +49,7 @@ void print(int i, int j) {
 			if (m[k][l] == 2)
 				printf("бс");
 			else if (m[k][l] == 0)
-				printf("бр");
+				printf("  ");
 			else
 				printf("в├");
 		}
